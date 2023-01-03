136ce66a9a3709e745a6d491396e6021ee1e175f
jorge0121 <jorgeluisdiz@gmail.com>
2023-01-03 06:31:29 -0500

    finished homework

---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { alerts } from "./Bienvenido";
+
+
+
+
+export default class Botones extends React.Component{
+    render(){
+        return <div>
+            <button  onClick={() => alert(alerts.m1)}>
+                Módulo 1
+            </button>
+            <button  onClick={() => alert(alerts.m2)}>
+                Módulo 2
+            </button>
+           
+        </div>
+    }
+}
