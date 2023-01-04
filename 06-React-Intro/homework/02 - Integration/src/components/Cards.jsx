8a9b0743ff44e7aa8c504f55e02983f777667d42
jorge0121 <jorgeluisdiz@gmail.com>
2023-01-04 06:08:54 -0500

    finished homework

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,15 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   return <div>
+      {characters.map((info)=> 
+      <Card  name= {info.name}
+             species = {info.species}
+             gender ={info.gender}
+             image ={info.image}
+             onClose = {() => info.onClose()}/> )
+      
+      }
+
+   </div>;
 }
